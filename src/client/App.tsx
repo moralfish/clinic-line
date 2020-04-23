10215791661b4394c19d9f56f0a50bbe87f8c5c0
nedal <nedal.elghamry@gmail.com>
2020-04-23 17:45:48 +0200

    init first demo

---
+++ src/client/App.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:3000";
+
+class App extends React.Component<IAppProps, IAppState> {
+	constructor(props: IAppProps) {
+		super(props);
+		this.state = {
+			clientNumber: 0
+		};
+	}
+
+	async componentDidMount() {
+		try {
+			const socket = socketIOClient(ENDPOINT);
+			socket.on("next-turn", data => {
+				this.setState({ clientNumber: data });
+				speechSynthesis.speak(new SpeechSynthesisUtterance("Turn number: "+data));
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	render() {
+		return (
+			<main className="container my-5">
+				<h1 className="text-primary text-center">Turn {this.state.clientNumber}!</h1>
+			</main>
+		);
+	}
+}
+
+export interface IAppProps {}
+
+export interface IAppState {
+	clientNumber: string;
+}
+
+export default App;
---
+++ src/server/server.ts
@@ -0,0 +1,34 @@
+import * as express from 'express';
+import apiRouter from './routes';
+import * as http from "http";
+const socketIo = require("socket.io");
+
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(apiRouter);
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+let interval;
+
+io.on("connection", (socket) => {
+    console.log("Client connected");
+    interval = setInterval(() => getNextTurn(socket), 5000);
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+        clearInterval(interval);
+    });
+});
+
+let turn = 1;
+const getNextTurn = socket => {
+    socket.emit("next-turn", turn);
+    turn++;
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
