0021b7c05103b27d23f4180b7d34f5441b1ee75c
nedal <nedal.elghamry@gmail.com>
2020-05-01 16:45:44 +0200

    websocket client port

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import socketIOClient from "socket.io-client";
 
-const ENDPOINT = "https://clinic-line.herokuapp.com:3000";
+// const port = process.env.PORT || 3000;
+// const ENDPOINT = "https://clinic-line.herokuapp.com:"+port;
 
 class App extends React.Component<IAppProps, IAppState> {
 	constructor(props: IAppProps) {
@@ -13,7 +14,7 @@ class App extends React.Component<IAppProps, IAppState> {
 
 	async componentDidMount() {
 		try {
-			const socket = socketIOClient(ENDPOINT);
+			const socket = socketIOClient();
 			socket.on("next-turn", data => {
 				this.setState({ clientNumber: data });
 				speechSynthesis.speak(new SpeechSynthesisUtterance("Turn number: "+data));
--- src/server/server.ts
+++ src/server/server.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(apiRouter);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 const io = socketIo(server);
